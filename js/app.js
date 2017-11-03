93f9afda915416099a6040f7b03b60f0d98669c9
Anda <ax.fiolle@gmail.com>
2017-11-03 15:43:28 +0100

    Mobile UX upgrade

--- js/app.js
+++ js/app.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
             scrollTop: ($( $(this).attr("href") ).offset().top)-65
         }, 500);
     return false;
-  });
+    });
+    
+    $(".scrollFoot").click(function(){
+        $("body,html").animate({
+            scrollTop: $( $(this).attr("href") ).offset().top
+        }, 500);
+    return false;
+    });
     
 });
