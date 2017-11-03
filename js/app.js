cbb437ca9edb30d017a7cdf5d3966a4dc06a241e
Anda <ax.fiolle@gmail.com>
2017-11-03 16:00:26 +0100

    rm empty lines

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
-
     $(".scrollTo,.homescroll").click(function(){
         $("body,html").animate({
             scrollTop: ($( $(this).attr("href") ).offset().top)-65
         }, 500);
     return false;
     });
-    
     $(".scrollFoot").click(function(){
         $("body,html").animate({
             scrollTop: $( $(this).attr("href") ).offset().top
         }, 500);
     return false;
     });
-    
 });
