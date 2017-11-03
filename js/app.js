58ae6e22cd090b3e003b1b37425e5a6c27f4819d
Anda <ax.fiolle@gmail.com>
2017-11-03 13:41:46 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $(".scrollTo").click(function(){
+    $(".scrollTo,.homescroll").click(function(){
         $("body,html").animate({
             scrollTop: ($( $(this).attr("href") ).offset().top)-65
         }, 500);
