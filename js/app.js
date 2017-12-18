a67bca8b65d5c1578b69b3876be6df4e1b9fc820
Anda <ax.fiolle@gmail.com>
2017-12-18 19:00:44 +0100

    update better ux

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-    $(".scrollTo,.homescroll").click(function(){
-        $("body,html").animate({
-            scrollTop: ($( $(this).attr("href") ).offset().top)-65
-        }, 500);
-    return false;
-    });
-    $(".scrollFoot").click(function(){
-        $("body,html").animate({
-            scrollTop: $( $(this).attr("href") ).offset().top
-        }, 750);
-    return false;
-    });
+  $(".scrollTo,.homescroll").click(function(){
+    $("body,html").animate({
+      scrollTop: ($( $(this).attr("href") ).offset().top)-65
+    }, 500);
+  return false;
+  });
+  $(".scrollFoot").click(function(){
+    $("body,html").animate({
+      scrollTop: $( $(this).attr("href") ).offset().top
+    }, 750);
+  return false;
+  });
 });
 /* Handmade by Axel Fiolle - ax.fiolle@gmail.com */
